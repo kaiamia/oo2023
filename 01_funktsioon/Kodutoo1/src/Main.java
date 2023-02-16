5142431cb9b97c66ed918cd4bec9b235c406613f
Kaia Mia Kalda <114983071+kaiamia@users.noreply.github.com>
2023-02-16 19:35:09 +0200

    Kodutöö1

---
+++ 01_funktsioon/Kodutoo1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        TemperatureConversionTable(20,40);
+
+    }
+
+    private static void TemperatureConversionTable(int CelsiusMin, int CelsiusMax) {
+        while (CelsiusMin <= CelsiusMax) {
+            double Fahrenheit = CelsiusMin*1.8+32;
+            String line = "Celsius: " + CelsiusMin + " -> Fahrenheit: " + String.format("%.2f", Fahrenheit);
+            System.out.print(line);
+            System.out.println(" ");
+            CelsiusMin++;
+        }
+    }
+}
